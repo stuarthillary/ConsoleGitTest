ccf056d0e2247451e17d999e189bfa8463c1ff44
Stuart Hillary <shillary.git@gmail.com>
2018-01-16 11:50:12 +0000

    Added stuff to persion

--- ConsoleGitTest/Person.cs
+++ ConsoleGitTest/Person.cs
@@ -2,7 +2,15 @@
 {
     public class Person
     {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
